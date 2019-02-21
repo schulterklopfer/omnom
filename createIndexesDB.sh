fa37a873215bb731efe1082acfa56bebcdbbec54
SKP <jash@schulterklopfer-productions.de>
2019-02-21 23:32:16 +0100

    Removed not needed scripts

--- createIndexesDB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DB_FILE="index.sqlite"
-
-if [ -e ${DB_FILE} ]; then
-	echo "DB found, creating indexes"
-	cat db_indexes.sql | sqlite3 $DB_FILE
-fi
--- initDB.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DB_FILE="index.sqlite"
-
-if [ ! -e ${DB_FILE} ]; then
-	echo "DB not found, creating..."
-	cat db.sql | sqlite3 $DB_FILE
-fi
